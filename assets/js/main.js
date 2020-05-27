b61e274941e83f69a24e1edef79f5b328137ff03
Dmitriy Elsukov <demndik@yandex.ru>
2020-05-27 23:32:20 +0300

    developed deployMethod, callMethod

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,13 +9,58 @@
         e.preventDefault();
         let button = $('button', this);
         let result = $('#result', this);
-        let data = {source: ($('#code', this).val() || false), resourceRent: ($('#resources', this).val() || false)};
+        let data = {
+            source: ($('#code', this).val() || false),
+            resourceRent: ($('#resources', this).val() || false)
+        };
         button.attr('disabled', true);
         result.html('');
         $.ajax({
             url: appURL + '/contracts/ecma/deployContract',
             method: 'POST',
             data: data,
+        })
+            .always(function (resp) {
+                result.html(`${toBeautyfyJSON(resp)}`);
+                button.attr('disabled', false);
+            });
+    });
+
+    $('#deploy-method').on('submit', function (e) {
+        e.preventDefault();
+        let button = $('button', this);
+        let result = $('#result', this);
+        let contractAddress = $('#address', this).val() || false;
+        let contractMethod = $('#method', this).val() || false;
+        let methodParams = $('#params', this).val() || [];
+
+        button.attr('disabled', true);
+        result.html('');
+        $.ajax({
+            url: appURL + '/contracts/ecma/deployMethod/' + contractAddress + '/' + contractMethod,
+            method: 'POST',
+            data: {'argsEncoded': JSON.stringify(methodParams)},
+        })
+            .always(function (resp) {
+                result.html(`${toBeautyfyJSON(resp)}`);
+                button.attr('disabled', false);
+            });
+    });
+
+    $('#call-method').on('submit', function (e) {
+        e.preventDefault();
+        let button = $('button', this);
+        let result = $('#result', this);
+        let contractAddress = $('#address', this).val() || false;
+        let contractMethod = $('#method', this).val() || false;
+        let methodParams = $('#params', this).val() || [];
+
+        button.attr('disabled', true);
+        result.html('');
+        $.ajax({
+            url: appURL + '/contracts/ecma/callMethod/' + contractAddress + '/' + contractMethod,
+            method: 'POST',
+            data: {'argsEncoded': JSON.stringify(methodParams)},
         })
             .always(function (resp) {
                 result.html(`${toBeautyfyJSON(resp)}`);
@@ -37,16 +82,12 @@
             });
     });
 
-    let toBeautyfyJSON = function(data){
+    let toBeautyfyJSON = function (data) {
         try {
             data = JSON.stringify(data, undefined, 2);
-        }catch (e) {
+        } catch (e) {
 
         }
         return data;
     };
-
-    let doRequest = function () {
-
-    }
 })();
